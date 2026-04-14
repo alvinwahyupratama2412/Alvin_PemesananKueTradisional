ed45ea3b74a8abda8f058f346f5688de0fd1a375
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 02:18:27 +0700

    Membuat code untuk menampilkan form lain dalam mainpanel

--- KueTradisional/Form1.cs
+++ KueTradisional/Form1.cs
@@ -16,5 +16,17 @@ namespace KueTradisional
         {
             InitializeComponent();
         }
+
+        public void loadform(object Form)
+        {
+            if (this.mainpanel.Controls.Count >0)
+                this.mainpanel.Controls.RemoveAt(0);
+            Form f = Form as Form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill;
+            this.mainpanel.Controls.Add(f);
+            this.mainpanel.Tag = f;
+            f.Show();
+        }
     }
 }
