a2d3b74b4968ca52392ec79859837b9db0334608
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 04:42:56 +0700

    Menambahkan label nama kue, nama pelanggan, jumlah, dan tanggal ambil, beserta textboxnya dan juga tombol update

--- KueTradisional/EditPesanan.Designer.cs
+++ KueTradisional/EditPesanan.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSimpantp = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblAmup = new System.Windows.Forms.Label();
+            this.txtupJumlah = new System.Windows.Forms.TextBox();
+            this.lblJumup = new System.Windows.Forms.Label();
+            this.lblnamapup = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtUpnama = new System.Windows.Forms.TextBox();
+            this.lblNamaup = new System.Windows.Forms.Label();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSimpantp
+            // 
+            this.btnSimpantp.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnSimpantp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSimpantp.ForeColor = System.Drawing.Color.White;
+            this.btnSimpantp.Location = new System.Drawing.Point(776, 391);
+            this.btnSimpantp.Name = "btnSimpantp";
+            this.btnSimpantp.Size = new System.Drawing.Size(0, 0);
+            this.btnSimpantp.TabIndex = 19;
+            this.btnSimpantp.Text = "Simpan";
+            this.btnSimpantp.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(250, 272);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 31);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // lblAmup
+            // 
+            this.lblAmup.AutoSize = true;
+            this.lblAmup.Font = new System.Drawing.Font("Lucida Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmup.Location = new System.Drawing.Point(27, 275);
+            this.lblAmup.Name = "lblAmup";
+            this.lblAmup.Size = new System.Drawing.Size(188, 28);
+            this.lblAmup.TabIndex = 17;
+            this.lblAmup.Text = "Tanggal Ambil";
+            // 
+            // txtupJumlah
+            // 
+            this.txtupJumlah.Location = new System.Drawing.Point(250, 187);
+            this.txtupJumlah.Name = "txtupJumlah";
+            this.txtupJumlah.Size = new System.Drawing.Size(328, 31);
+            this.txtupJumlah.TabIndex = 16;
+            // 
+            // lblJumup
+            // 
+            this.lblJumup.AutoSize = true;
+            this.lblJumup.Font = new System.Drawing.Font("Lucida Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJumup.Location = new System.Drawing.Point(27, 190);
+            this.lblJumup.Name = "lblJumup";
+            this.lblJumup.Size = new System.Drawing.Size(97, 28);
+            this.lblJumup.TabIndex = 15;
+            this.lblJumup.Text = "Jumlah";
+            // 
+            // lblnamapup
+            // 
+            this.lblnamapup.AutoSize = true;
+            this.lblnamapup.Font = new System.Drawing.Font("Lucida Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnamapup.Location = new System.Drawing.Point(27, 101);
+            this.lblnamapup.Name = "lblnamapup";
+            this.lblnamapup.Size = new System.Drawing.Size(217, 28);
+            this.lblnamapup.TabIndex = 14;
+            this.lblnamapup.Text = "Nama Pelanggan";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(250, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 33);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // txtUpnama
+            // 
+            this.txtUpnama.Location = new System.Drawing.Point(250, 101);
+            this.txtUpnama.Name = "txtUpnama";
+            this.txtUpnama.Size = new System.Drawing.Size(328, 31);
+            this.txtUpnama.TabIndex = 12;
+            // 
+            // lblNamaup
+            // 
+            this.lblNamaup.AutoSize = true;
+            this.lblNamaup.Font = new System.Drawing.Font("Lucida Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNamaup.Location = new System.Drawing.Point(27, 25);
+            this.lblNamaup.Name = "lblNamaup";
+            this.lblNamaup.Size = new System.Drawing.Size(136, 28);
+            this.lblNamaup.TabIndex = 11;
+            this.lblNamaup.Text = "Nama Kue";
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackColor = System.Drawing.Color.Olive;
+            this.btnUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUp.ForeColor = System.Drawing.Color.White;
+            this.btnUp.Location = new System.Drawing.Point(776, 391);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(150, 50);
+            this.btnUp.TabIndex = 20;
+            this.btnUp.Text = "Update";
+            this.btnUp.UseVisualStyleBackColor = false;
+            // 
+            // EditPesanan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(953, 467);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnSimpantp);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblAmup);
+            this.Controls.Add(this.txtupJumlah);
+            this.Controls.Add(this.lblJumup);
+            this.Controls.Add(this.lblnamapup);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtUpnama);
+            this.Controls.Add(this.lblNamaup);
+            this.Name = "EditPesanan";
             this.Text = "EditPesanan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSimpantp;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblAmup;
+        private System.Windows.Forms.TextBox txtupJumlah;
+        private System.Windows.Forms.Label lblJumup;
+        private System.Windows.Forms.Label lblnamapup;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtUpnama;
+        private System.Windows.Forms.Label lblNamaup;
+        private System.Windows.Forms.Button btnUp;
     }
 }
