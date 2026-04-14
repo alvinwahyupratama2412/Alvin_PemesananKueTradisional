07ea379e96d613f1479061cf9df66bc07fe24922
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 02:10:37 +0700

    Menambahkan Label Daftar Kue pada form Kue, dan Label daftar Pesanan pada form Pesanan

--- KueTradisional/Kue.Designer.cs
+++ KueTradisional/Kue.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbljudulk = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbljudulk
+            // 
+            this.lbljudulk.AutoSize = true;
+            this.lbljudulk.Font = new System.Drawing.Font("Lucida Sans", 16.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbljudulk.ForeColor = System.Drawing.Color.DimGray;
+            this.lbljudulk.Location = new System.Drawing.Point(12, 9);
+            this.lbljudulk.Name = "lbljudulk";
+            this.lbljudulk.Size = new System.Drawing.Size(262, 49);
+            this.lbljudulk.TabIndex = 1;
+            this.lbljudulk.Text = "Daftar Kue";
             // 
             // Kue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1345, 848);
+            this.Controls.Add(this.lbljudulk);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Kue";
             this.Text = "Kue";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbljudulk;
     }
 }
--- KueTradisional/Pesanan.Designer.cs
+++ KueTradisional/Pesanan.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbljudulp = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbljudulp
+            // 
+            this.lbljudulp.AutoSize = true;
+            this.lbljudulp.Font = new System.Drawing.Font("Lucida Sans", 16.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbljudulp.ForeColor = System.Drawing.Color.DimGray;
+            this.lbljudulp.Location = new System.Drawing.Point(12, 9);
+            this.lbljudulp.Name = "lbljudulp";
+            this.lbljudulp.Size = new System.Drawing.Size(362, 49);
+            this.lbljudulp.TabIndex = 0;
+            this.lbljudulp.Text = "Daftar Pesanan";
             // 
             // Pesanan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1345, 848);
+            this.Controls.Add(this.lbljudulp);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Pesanan";
             this.Text = "Pesanan";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbljudulp;
     }
 }
