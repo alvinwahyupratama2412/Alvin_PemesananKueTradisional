d351edbc60e03a4bde8bce2f88988ee470138cf3
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 03:11:06 +0700

    Mengubah gaya kursor saat menyentuh button

--- KueTradisional/Form1.Designer.cs
+++ KueTradisional/Form1.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.panelside = new System.Windows.Forms.Panel();
-            this.panelheader = new System.Windows.Forms.Panel();
-            this.mainpanel = new System.Windows.Forms.Panel();
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.btnKue = new System.Windows.Forms.Button();
-            this.btnPesanan = new System.Windows.Forms.Button();
             this.logo = new System.Windows.Forms.PictureBox();
-            this.btnClose = new System.Windows.Forms.Button();
+            this.btnPesanan = new System.Windows.Forms.Button();
+            this.btnKue = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.panelheader = new System.Windows.Forms.Panel();
             this.lbltitle = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.mainpanel = new System.Windows.Forms.Panel();
             this.panelside.SuspendLayout();
-            this.panelheader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.panelheader.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelside
@@ -55,6 +55,61 @@
             this.panelside.Size = new System.Drawing.Size(358, 919);
             this.panelside.TabIndex = 0;
             // 
+            // logo
+            // 
+            this.logo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logo.Image = global::KueTradisional.Properties.Resources.Logo_sistem_pemesanan_kue_tradisional__3_;
+            this.logo.Location = new System.Drawing.Point(49, 0);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(270, 270);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            // 
+            // btnPesanan
+            // 
+            this.btnPesanan.BackColor = System.Drawing.Color.Gray;
+            this.btnPesanan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPesanan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesanan.Font = new System.Drawing.Font("Lucida Sans", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesanan.ForeColor = System.Drawing.Color.White;
+            this.btnPesanan.Location = new System.Drawing.Point(0, 427);
+            this.btnPesanan.Name = "btnPesanan";
+            this.btnPesanan.Size = new System.Drawing.Size(358, 106);
+            this.btnPesanan.TabIndex = 2;
+            this.btnPesanan.Text = "Pesanan";
+            this.btnPesanan.UseVisualStyleBackColor = false;
+            this.btnPesanan.Click += new System.EventHandler(this.btnPesanan_Click);
+            // 
+            // btnKue
+            // 
+            this.btnKue.BackColor = System.Drawing.Color.Gray;
+            this.btnKue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnKue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKue.Font = new System.Drawing.Font("Lucida Sans", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKue.ForeColor = System.Drawing.Color.White;
+            this.btnKue.Location = new System.Drawing.Point(0, 326);
+            this.btnKue.Name = "btnKue";
+            this.btnKue.Size = new System.Drawing.Size(358, 106);
+            this.btnKue.TabIndex = 1;
+            this.btnKue.Text = "Kue";
+            this.btnKue.UseVisualStyleBackColor = false;
+            this.btnKue.Click += new System.EventHandler(this.btnKue_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Gray;
+            this.btnConnect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConnect.Font = new System.Drawing.Font("Lucida Handwriting", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(38, 823);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(271, 59);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect Database";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            // 
             // panelheader
             // 
             this.panelheader.BackColor = System.Drawing.Color.Silver;
@@ -63,82 +118,8 @@
             this.panelheader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelheader.Location = new System.Drawing.Point(0, 0);
             this.panelheader.Name = "panelheader";
-            this.panelheader.Size = new System.Drawing.Size(1729, 44);
+            this.panelheader.Size = new System.Drawing.Size(1668, 44);
             this.panelheader.TabIndex = 1;
-            // 
-            // mainpanel
-            // 
-            this.mainpanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainpanel.Location = new System.Drawing.Point(358, 44);
-            this.mainpanel.Name = "mainpanel";
-            this.mainpanel.Size = new System.Drawing.Size(1371, 919);
-            this.mainpanel.TabIndex = 2;
-            // 
-            // btnConnect
-            // 
-            this.btnConnect.BackColor = System.Drawing.Color.Gray;
-            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnConnect.Font = new System.Drawing.Font("Lucida Handwriting", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnect.ForeColor = System.Drawing.Color.White;
-            this.btnConnect.Location = new System.Drawing.Point(37, 860);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(265, 47);
-            this.btnConnect.TabIndex = 0;
-            this.btnConnect.Text = "Connect Database";
-            this.btnConnect.UseVisualStyleBackColor = false;
-            // 
-            // btnKue
-            // 
-            this.btnKue.BackColor = System.Drawing.Color.Gray;
-            this.btnKue.Cursor = System.Windows.Forms.Cursors.Default;
-            this.btnKue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKue.Font = new System.Drawing.Font("Lucida Sans", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnKue.ForeColor = System.Drawing.Color.White;
-            this.btnKue.Location = new System.Drawing.Point(0, 272);
-            this.btnKue.Name = "btnKue";
-            this.btnKue.Size = new System.Drawing.Size(358, 106);
-            this.btnKue.TabIndex = 1;
-            this.btnKue.Text = "Kue";
-            this.btnKue.UseVisualStyleBackColor = false;
-            this.btnKue.Click += new System.EventHandler(this.btnKue_Click);
-            // 
-            // btnPesanan
-            // 
-            this.btnPesanan.BackColor = System.Drawing.Color.Gray;
-            this.btnPesanan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPesanan.Font = new System.Drawing.Font("Lucida Sans", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPesanan.ForeColor = System.Drawing.Color.White;
-            this.btnPesanan.Location = new System.Drawing.Point(3, 375);
-            this.btnPesanan.Name = "btnPesanan";
-            this.btnPesanan.Size = new System.Drawing.Size(358, 106);
-            this.btnPesanan.TabIndex = 2;
-            this.btnPesanan.Text = "Pesanan";
-            this.btnPesanan.UseVisualStyleBackColor = false;
-            this.btnPesanan.Click += new System.EventHandler(this.btnPesanan_Click);
-            // 
-            // logo
-            // 
-            this.logo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.logo.Image = global::KueTradisional.Properties.Resources.Logo_sistem_pemesanan_kue_tradisional__3_;
-            this.logo.Location = new System.Drawing.Point(49, 0);
-            this.logo.Name = "logo";
-            this.logo.Size = new System.Drawing.Size(270, 270);
-            this.logo.TabIndex = 0;
-            this.logo.TabStop = false;
-            // 
-            // btnClose
-            // 
-            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Font = new System.Drawing.Font("Lucida Sans", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(1682, 0);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(47, 44);
-            this.btnClose.TabIndex = 3;
-            this.btnClose.Text = "X";
-            this.btnClose.UseVisualStyleBackColor = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // lbltitle
             // 
@@ -151,12 +132,36 @@
             this.lbltitle.TabIndex = 2;
             this.lbltitle.Text = "Pemesanan Kue Tradisional";
             // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Lucida Sans", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(1621, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(47, 44);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // mainpanel
+            // 
+            this.mainpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainpanel.Location = new System.Drawing.Point(358, 44);
+            this.mainpanel.Name = "mainpanel";
+            this.mainpanel.Size = new System.Drawing.Size(1310, 919);
+            this.mainpanel.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(1729, 963);
+            this.ClientSize = new System.Drawing.Size(1668, 963);
             this.Controls.Add(this.mainpanel);
             this.Controls.Add(this.panelside);
             this.Controls.Add(this.panelheader);
@@ -165,9 +170,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.panelside.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.panelheader.ResumeLayout(false);
             this.panelheader.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.ResumeLayout(false);
 
         }
