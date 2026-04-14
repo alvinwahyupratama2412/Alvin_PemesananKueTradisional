43cf2b06c076f60d353d9ad34877d94821fe9af4
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 01:31:11 +0700

    Membuat form serta menambahkan panel side dan panel header

--- KueTradisional/Form1.Designer.cs
+++ KueTradisional/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelside = new System.Windows.Forms.Panel();
+            this.panelheader = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelside
+            // 
+            this.panelside.BackColor = System.Drawing.Color.Gray;
+            this.panelside.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelside.Location = new System.Drawing.Point(0, 44);
+            this.panelside.Name = "panelside";
+            this.panelside.Size = new System.Drawing.Size(358, 919);
+            this.panelside.TabIndex = 0;
+            // 
+            // panelheader
+            // 
+            this.panelheader.BackColor = System.Drawing.Color.Silver;
+            this.panelheader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelheader.Location = new System.Drawing.Point(0, 0);
+            this.panelheader.Name = "panelheader";
+            this.panelheader.Size = new System.Drawing.Size(1729, 44);
+            this.panelheader.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1729, 963);
+            this.Controls.Add(this.panelside);
+            this.Controls.Add(this.panelheader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelside;
+        private System.Windows.Forms.Panel panelheader;
     }
 }
 
