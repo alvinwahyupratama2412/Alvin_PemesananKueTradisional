4e9e6fd156f0f082168d7b41bc5a985475cb0dcc
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 08:05:05 +0700

    Membuat code untuk menambahkan kolom pada Datagridview menggunakan button Tampil

--- KueTradisional/Pesanan.cs
+++ KueTradisional/Pesanan.cs
@@ -43,7 +43,49 @@ namespace KueTradisional
 
         private void btnTampilp_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                dataGridView1.Columns.Add("PesananID", "PesananID");
+                dataGridView1.Columns.Add("NamaKue", "NamaKue");
+                dataGridView1.Columns.Add("NamaPelanggan", "NamaPelanggan");
+                dataGridView1.Columns.Add("Jumlah", "Jumlah");
+                dataGridView1.Columns.Add("TanggalPesan", "TanggalPesan");
+                dataGridView1.Columns.Add("TanggalAmbil", "TanggalAmbil");
+                dataGridView1.Columns.Add("TotalHarga", "TotalHarga");
+
+                string query = @" SELECT p.PesananID,k.NamaKue,p.NamaPelanggan,p.Jumlah,p.TanggalPesan,p.TanggalAmbil,p.TotalHarga
+                FROM Pesanan p
+                JOIN Kue k ON p.KueID = k.KueID";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["PesananID"].ToString(),
+                        reader["NamaKue"].ToString(),
+                        reader["NamaPelanggan"].ToString(),
+                        reader["Jumlah"].ToString(),
+                        reader["TanggalPesan"].ToString(),
+                        reader["TanggalAmbil"].ToString(),
+                        reader["TotalHarga"].ToString()
+                    );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data : " + ex.Message);
+            }
         }
     }
 }
