a6a01bfcc050ab3cc2d96fcbe17614b73891788b
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-15 11:35:48 +0700

    Membuat Koneksi dengan database pada form editPesanan

--- KueTradisional/EditPesanan.cs
+++ KueTradisional/EditPesanan.cs
@@ -7,15 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 
 namespace KueTradisional
 {
     public partial class EditPesanan : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=LAPTOP-5BS6SFFN\\ALVIN;Initial Catalog=KueTradisionalDB;Integrated Security=True";
+
         public EditPesanan()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
         private void EditPesanan_Load(object sender, EventArgs e)
