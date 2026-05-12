dedceb85cdf81c4cd6ce792d6185ab1b1de523d5
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-05-13 06:22:15 +0700

    Membuat koneksi dengan database pada form EditPelanggan

--- KueTradisional/EditPelanggan.cs
+++ KueTradisional/EditPelanggan.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace KueTradisional
 {
     public partial class EditPelanggan : Form
     {
+        private readonly string connectionString =
+           "Data Source=DESKTOP-RF6R80E\\ALVINWAHYU12;Initial Catalog=KueTradisionalDB;Integrated Security=True";
+
         public EditPelanggan()
         {
             InitializeComponent();
