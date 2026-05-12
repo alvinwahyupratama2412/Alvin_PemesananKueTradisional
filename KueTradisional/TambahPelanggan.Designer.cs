0324c403e2ba5033c9e2931bb901ed2b7ad98860
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-05-13 00:25:55 +0700

    Menambahkan label nama dan no hp, textbox Nmaa dan no Hp, dan button Tambah

--- KueTradisional/TambahPelanggan.Designer.cs
+++ KueTradisional/TambahPelanggan.Designer.cs
@@ -28,19 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblnm = new System.Windows.Forms.Label();
+            this.lblno = new System.Windows.Forms.Label();
+            this.txtnm = new System.Windows.Forms.TextBox();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.btntambahpl = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblnm
+            // 
+            this.lblnm.AutoSize = true;
+            this.lblnm.Location = new System.Drawing.Point(92, 81);
+            this.lblnm.Name = "lblnm";
+            this.lblnm.Size = new System.Drawing.Size(80, 25);
+            this.lblnm.TabIndex = 0;
+            this.lblnm.Text = "Nama  ";
+            // 
+            // lblno
+            // 
+            this.lblno.AutoSize = true;
+            this.lblno.Location = new System.Drawing.Point(92, 162);
+            this.lblno.Name = "lblno";
+            this.lblno.Size = new System.Drawing.Size(72, 25);
+            this.lblno.TabIndex = 1;
+            this.lblno.Text = "No Hp";
+            // 
+            // txtnm
+            // 
+            this.txtnm.Location = new System.Drawing.Point(208, 78);
+            this.txtnm.Name = "txtnm";
+            this.txtnm.Size = new System.Drawing.Size(268, 31);
+            this.txtnm.TabIndex = 2;
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(208, 159);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(268, 31);
+            this.txtno.TabIndex = 3;
+            // 
+            // btntambahpl
+            // 
+            this.btntambahpl.Location = new System.Drawing.Point(785, 416);
+            this.btntambahpl.Name = "btntambahpl";
+            this.btntambahpl.Size = new System.Drawing.Size(114, 45);
+            this.btntambahpl.TabIndex = 4;
+            this.btntambahpl.Text = "Tambah";
+            this.btntambahpl.UseVisualStyleBackColor = true;
             // 
             // TambahPelanggan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(945, 498);
+            this.Controls.Add(this.btntambahpl);
+            this.Controls.Add(this.txtno);
+            this.Controls.Add(this.txtnm);
+            this.Controls.Add(this.lblno);
+            this.Controls.Add(this.lblnm);
             this.Name = "TambahPelanggan";
             this.Text = "TambahPelanggan";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblnm;
+        private System.Windows.Forms.Label lblno;
+        private System.Windows.Forms.TextBox txtnm;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.Button btntambahpl;
     }
 }
