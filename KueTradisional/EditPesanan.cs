735c6962f622d5e78aa5ecd9e883db1f1840fc64
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-06-24 18:24:48 +0700

    Membuat  Fungsi batasan input untuk karakter khusus dan batasan input angka pada fitur edit pesanan

--- KueTradisional/EditPesanan.cs
+++ KueTradisional/EditPesanan.cs
@@ -24,6 +24,25 @@ namespace KueTradisional
             
         }
 
+        private void TextHuruf_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) &&
+                !char.IsWhiteSpace(e.KeyChar) &&
+                !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextAngka_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) &&
+                !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
         private void EditPesanan_Load(object sender, EventArgs e)
         {
             LoadKue();
