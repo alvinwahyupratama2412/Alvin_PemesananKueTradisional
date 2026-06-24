78c58055987f9700ac89c0b86dae933f5aa16b34
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-06-24 19:44:17 +0700

    Menambahkan koneksi database ke DAL.cs

--- KueTradisional/DAL.cs
+++ KueTradisional/DAL.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace KueTradisional
 {
     internal class DAL
     {
+        private readonly string connectionString =
+            "Data Source=DESKTOP-RF6R80E\\ALVINWAHYU12;Initial Catalog=KueTradisionalDB;Integrated Security=True";
     }
 }
