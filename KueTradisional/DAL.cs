22d2e42e30c669d9a709d9037f50a45d34841f7b
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-06-24 19:46:05 +0700

    Membuat void SimpanLog yang berfungsi untuk mencatat Logerror ke tabel LogError

--- KueTradisional/DAL.cs
+++ KueTradisional/DAL.cs
@@ -12,5 +12,25 @@ namespace KueTradisional
     {
         private readonly string connectionString =
             "Data Source=DESKTOP-RF6R80E\\ALVINWAHYU12;Initial Catalog=KueTradisionalDB;Integrated Security=True";
+
+        public void SimpanLog(string pesan, string procedure, int line)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO LogError (ErrorMessage, ErrorProcedure, ErrorLine) VALUES (@ErrorMessage, @ErrorProcedure, @ErrorLine)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ErrorMessage", pesan);
+                    cmd.Parameters.AddWithValue("@ErrorProcedure", procedure);
+                    cmd.Parameters.AddWithValue("@ErrorLine", line);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
+
+
 }
