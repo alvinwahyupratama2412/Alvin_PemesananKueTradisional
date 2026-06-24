8a4aa043d2cf88b675f8e825d4069ef3ce298dcb
Alvin Wahyu P <alvinwahyu1224@gmail.com>
2026-06-24 20:10:12 +0700

    Menambahkan FormReportPesanan

---
+++ KueTradisional/FormReportPesanan.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace KueTradisional
+{
+    partial class FormReportPesanan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormReportPesanan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 630);
+            this.Name = "FormReportPesanan";
+            this.Text = "FormReportPesanan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
